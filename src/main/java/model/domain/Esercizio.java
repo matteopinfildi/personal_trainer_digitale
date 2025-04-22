f86b2d2d2a23f7f3a1d51c960a2d01a01b5e4066
matteopinfildi <pinfildimatteo@gmail.com>
2025-04-22 16:12:07 +0200

    prima push

---
+++ src/main/java/model/domain/Esercizio.java
@@ -0,0 +1,48 @@
+package model.domain;
+
+public class Esercizio {
+    private int codiceEs;
+    private String nome;
+    private String descrizione;
+    private int numSerie;
+    private int ripetizioni;
+
+    public Esercizio(int codiceEs, String nome, String descrizione, int numSerie, int ripetizioni) {
+        this.codiceEs = codiceEs;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.numSerie = numSerie;
+        this.ripetizioni = ripetizioni;
+    }
+
+    public int getCodiceEs() {
+        return codiceEs;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public int getNumSerie() {
+        return numSerie;
+    }
+
+    public int getRipetizioni() {
+        return ripetizioni;
+    }
+
+    @Override
+    public String toString() {
+        return "Esercizio{" +
+                "codiceEs=" + codiceEs +
+                ", nome='" + nome + '\'' +
+                ", descrizione='" + descrizione + '\'' +
+                ", numSerie=" + numSerie +
+                ", ripetizioni=" + ripetizioni +
+                '}';
+    }
+}
